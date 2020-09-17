9d234bbbd7116da0c21ad91263f5c81afc30039b
GregoryValsaint <gvalsaint@live.fr>
2020-09-17 09:50:12 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [user, setUser] = useState(null)
+    const [newUsername, setNewUsername] =useState([])
+    const [message, setMessage] = useState("hello World")
+    const [newMessage, setNewMessage] = useState([])
+    useEffect(authentication, [])
+
+ // user
+    function setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+
+    function getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i <ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+ function authentication() {
+      if (getCookie("authToken")){
+          axios.post("https://api.messenger.codecolliders.com/getUser",
+              {
+                  authKey: getCookie("authToken")
+              }
+          ).then(response => {
+              setUser(response.data);
+            })
+      } else {
+          axios.post("https://api.messenger.codecolliders.com/createUser",
+              {}
+          ).then(response => {
+              console.log(response);
+              setCookie("authToken", response.data.authKey, 1)
+              setUser(response.data)
+          })
+      }
+ }
+
+    function inputUser(event) {
+        setNewUsername(event.target.value)
+    }
+
+    function changeUsername(event) {
+        event.preventDefault()
+        axios.post("https://api.messenger.codecolliders.com/changeUsername",
+            {
+                authKey: getCookie("authToken"),
+                username: newUsername
+            }).then(response => {
+            setUser(response.data)
+            console.log(user)
+        })
+    }
+
+    function getUser() {
+        if (user) {
+            return <h1>{user.username}</h1>
+        }
+    }
+
+
+    // Message
+function inputMessage(event) {
+    setNewMessage(event.target.value)
+}
+
+ function sendMessage (event) {
+      event.preventDefault()
+        axios.post("https://api.messenger.codecolliders.com/sendMessage",
+             {
+                 authKey: getCookie("authToken"),
+                 text: newMessage,
+                 to: 0
+         }).then(response => {
+             setMessage(response.data)
+            console.log(message)
+         })
+ }
+
+    function getMessage() {
+        if (user) {
+            return <div>{user.message}</div>
+        }
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          {getUser()}
+            <form onSubmit={sendMessage}>
+              <input type="text" value={newMessage} onChange={inputMessage}/>
+              <input type="submit" value="Envoyer" />
+            </form>
+            <form onSubmit={changeUsername}>
+                <input type="text" value={newUsername} onChange={inputUser}/>
+                <input type="submit" value="Envoyer" />
+            </form>
+
+        </header>
+      </div>
   );
 }
 
